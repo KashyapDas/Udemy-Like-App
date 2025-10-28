7c74170ebb6f1b8554903a58b9b40cc265a82d70
Kashyap Das <kashyap.education2234@gmail.com>
2025-10-28 17:12:01 +0530

    create the course schema

--- database/sellerDb.js
+++ database/sellerDb.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { Schema } = require("zod");
 
 // designing the schema 
 const sellerAccSchema = new mongoose.Schema({
@@ -46,11 +47,48 @@ const addressSchemaSeller = new mongoose.Schema({
         required : true
     },
     street : {
-        type : String,
+        type : String, // value is "optional" 
     }
 })
 
+const courseSchema = new mongoose.Schema({
+    c_name : {
+        type : String,
+        required : true
+    },
+    c_desc : {
+        type : String,
+        required : true
+    },
+    c_img : {
+        type : c_imgSchema,
+        required : true
+    },
+    c_price : {
+        type : String,
+        required : true
+    },
+    c_duration : {
+        type : String,
+        required : true
+    },
+    c_validity : {
+        type : String,
+        required : true
+    }
+})
+// schema for images
+const c_imgSchema = new mongoose.Schema({
+    filename : String,
+    mimetype : String,
+    data : Buffer
+});
+
 // design the model
 const sellerModel = new mongoose.model("sellertable",sellerAccSchema);
+const courseModel = new mongoose.model("coursetable",courseSchema);
 
-module.exports = sellerModel;
+module.exports = {
+    sellerModel,
+    courseModel
+};
